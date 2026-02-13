87df1af6f80e5e464eddc4a1b6ccf9dd9fd228d7
Expresadd <rozovsky.o@yandex.ru>
2026-02-13 18:53:06 +0300

    Delete nike directory

--- nike/js/burger.js
+++
@@ -1,9 +0,0 @@
-// const burger = document.querySelector(".burger");
-
-
-// burger.addEventListener("click", () => {
-// 	burger.classList.add("burger--open");
-// });
-export function log() {
-	console.log(123);
-}
